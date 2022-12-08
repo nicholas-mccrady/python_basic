04710f56eb75b4b5c4dbc5fecb82b6843a8a492c
nicholas-mccrady <nicholas.mccrady@gmail.com>
2022-12-08 16:10:29 -0600

    creation of python files demonstating the basics

---
+++ basics_getting_started.py
@@ -0,0 +1,8 @@
+# this file demonsrates some of the key python modules
+
+# printing to the console
+# note you can use single or double quotes just stay consistant with your choice
+# use double quotes for strings likley to contain single quote caracters
+# use single for string literals
+print("hello world") 
+print('hello world')
---
+++ variables_simple_data_types.py
@@ -0,0 +1,6 @@
+# this file will demonstrate the basic variables and data types that exist in python
+
+# name a variable and assign ita value
+message = "hello world"
+# now print the variable to the console
+print(message) 
