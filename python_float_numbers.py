96d37fc538c9e48aaca5540f495b79612fd96f5e
nicholas-mccrady <nicholas.mccrady@gmail.com>
2022-12-09 09:34:57 -0600

    finished integers

--- python_float_numbers.py
+++ python_float_numbers.py
@@ -1,1 +1,20 @@
-#
+# any number in python that has a decimal point and most other languages is called a float
+# mostly it refers to any number with a decimal point and the fact that it can occur at any position in the
+# number.
+
+# a floating number
+my_floating_number = 2.0
+
+# doing math with a floating number
+my_second_floating_number = 3.0
+first_result = my_floating_number + my_second_floating_number
+print (first_result)
+
+# beware that some mathmatic operations will produce unexpected results and to program accordingly
+unexpected_number1 = 0.2
+unexpected_number2 = 0.1
+unexpected_result = unexpected_number1 + unexpected_number2
+print (unexpected_result)
+# note that this operation produces alot of decimal places on the out put and this is due to python trying to 
+# represent the number as acuratly as it can
+
